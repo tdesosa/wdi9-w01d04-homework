ad12ee8392b32f956f5347075753afe9abe82c66
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-13 17:05:00 -0600

    Commit 2 - Panlindrome

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //console.log("Test");
 
-// VERBAL QUESTIONS
+// 1. VERBAL QUESTIONS
 
 // Q1) a parameter is a variable in a method definition
 // while an argument is the actual value of the variable
@@ -13,3 +13,18 @@
 // Q3) when a value is returned within a function
 // the function will be "exited". 
 
+// 2. PALINDROME AGAIN
+
+const checkPalindrome = (word) => {
+    const lowerWord = word.toLowerCase("");
+    const reverseWord = lowerWord.split("").reverse("").join("");
+    reverseWord.toLowerCase("");
+    if(lowerWord === reverseWord){
+        return true;
+    } else {
+        return false;
+    }
+ }
+ 
+ console.log(checkPalindrome("Radar"));
+ console.log(checkPalindrome("Borscht"));
