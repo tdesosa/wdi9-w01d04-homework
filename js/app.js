42846b94831b36cadd91e4d976efdfd274e58c35
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-13 16:46:46 -0600

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//console.log("Test");
+
+// VERBAL QUESTIONS
+
+// Q1) a parameter is a variable in a method definition
+// while an argument is the actual value of the variable
+// that gets passed to a function.
+
+// Q2) console.log will print a message to the console
+// while return will exit a function and return a specific
+// value.
+
+// Q3) when a value is returned within a function
+// the function will be "exited". 
+
