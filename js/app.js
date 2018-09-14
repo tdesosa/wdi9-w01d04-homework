47198758ac29f80ecdec38c02cb56cd337449826
Tomas DeSosa <TDeSosa@TD-MacAir.local>
2018-09-13 18:39:43 -0600

    Commit 5 - Sum Array

--- js/app.js
+++ js/app.js
@@ -52,3 +52,15 @@ const checkPalindrome = (word) => {
  }
 
  calculateSide(8, 6);
+
+ // 5. SUM ARRAY
+
+ const sumArray = (arr) => {
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i];
+    }
+    console.log(sum);
+ }
+
+ sumArray([1, 2, 3, 4, 5, 6]);
